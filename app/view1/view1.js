87e247bc2857f900042af667a0d0a3e922b17c5b
Øystein Kjærnet <kjarnet@gmail.com>
2015-03-07 13:56:01 +0100

    Got something running.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,13 +2,44 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller : 'View1Ctrl'
+    });
+  }])
+  .factory('calculateLoan', ['$http', function ($http) {
+    return function (params) {
+      return $http.jsonp(
+        "https://cfs-ws-itera.cicero.no/cfp/6/ws/rest/calculator/calculateLoan?_jsonp=JSON_CALLBACK",
+        {method: 'GET', params: params}
+      );
+    };
+  }])
+  .controller('View1Ctrl', ['$scope', 'calculateLoan',
+    function ($scope, calculateLoan) {
+      $scope.inputParameters = {
+        amount  : 2560000,
+        downPay : 20,
+        interest: 3.45
+      };
 
-.controller('View1Ctrl', [function() {
+      $scope.output = {
+        monthly: 0
+      };
 
-}]);
+
+      $scope.$watch('inputParameters', function (newval) {
+        var arg = {
+          loanRaisingMonth         : 9,
+          loanRaisingYear          : moment().year(),
+          principalAmount          : newval.amount,
+          annualNominalInterestRate: 3.45,
+          totalNumberOfPayments    : 240
+        };
+        calculateLoan(arg).success(function (data) {
+          console.log('fetched data: ', data);
+        });
+      }, true);
+
+    }]);
