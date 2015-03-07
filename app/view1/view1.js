eab15f51286773d5ca90a4dd978c4c66b2baf0c0
Øystein Kjærnet <kjarnet@gmail.com>
2015-03-07 20:23:06 +0100

    Minimal working solution.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -20,7 +20,7 @@ angular.module('myApp.view1', ['ngRoute'])
     function ($scope, calculateLoan) {
       $scope.inputParameters = {
         amount  : 2560000,
-        downPay : 20,
+        downPayYears : 20,
         interest: 3.45
       };
 
@@ -28,17 +28,19 @@ angular.module('myApp.view1', ['ngRoute'])
         monthly: 0
       };
 
-
       $scope.$watch('inputParameters', function (newval) {
+        var now = moment();
+        var downPayNum = newval.downPayYears * 12;
         var arg = {
-          loanRaisingMonth         : 9,
-          loanRaisingYear          : moment().year(),
+          loanRaisingMonth         : now.month(),
+          loanRaisingYear          : now.year(),
           principalAmount          : newval.amount,
-          annualNominalInterestRate: 3.45,
-          totalNumberOfPayments    : 240
+          annualNominalInterestRate: newval.interest,
+          totalNumberOfPayments    : downPayNum
         };
         calculateLoan(arg).success(function (data) {
           console.log('fetched data: ', data);
+          $scope.output.monthly = Math.round(data.amortizationSchedule[0].payment);
         });
       }, true);
 
