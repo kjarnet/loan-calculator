16232db51d2ddc42b849e0999dbb7acdddedf2d5
Øystein Kjærnet <kjarnet@gmail.com>
2015-03-12 16:35:42 +0100

    Refactored to more sane division between controller and directives.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,13 +2,44 @@
 
 angular.module('loanCalculator.controllers', [])
 
-  .controller('View1Ctrl', ['$scope',
+  .controller('View1Ctrl', ['$scope', 'calculateLoanService', function ($scope, calculateLoanService) {
+    $scope.inputParameters = {
+      amount      : 2560000,
+      downPayYears: 20,
+      interest    : 3.45
+    };
 
-    function ($scope) {
-      $scope.inputParameters = {
-        amount  : 2560000,
-        downPayYears : 20,
-        interest: 3.45
-      };
+    $scope.output = {
+      monthlyPayments: 0
+    };
 
-    }]);
+    function calculateLoan(serviceArgs) {
+      calculateLoanService(serviceArgs).success(function (data) {
+        console.log('fetched data: ', data);
+        $scope.output.monthlyPayments = Math.round(data.amortizationSchedule[0].payment);
+      });
+    }
+
+    var now = moment();
+    var args = {
+      loanRaisingMonth         : now.month(),
+      loanRaisingYear          : now.year(),
+      principalAmount          : $scope.inputParameters.amount,
+      annualNominalInterestRate: $scope.inputParameters.interest,
+      totalNumberOfPayments    : $scope.inputParameters.downPayYears * 12
+    };
+
+    $scope.$watch('inputParameters.amount', function (newVal) {
+      args.principalAmount = newVal;
+      calculateLoan(args);
+    });
+    $scope.$watch('inputParameters.interest', function (newVal) {
+      args.annualNominalInterestRate = newVal;
+      calculateLoan(args);
+    });
+    $scope.$watch('inputParameters.downPayYears', function (newVal) {
+      args.totalNumberOfPayments = newVal * 12;
+      calculateLoan(args);
+    });
+
+  }]);
--- app/js/directives.js
+++ app/js/directives.js
@@ -2,42 +2,14 @@
 
 angular.module('loanCalculator.directives', ['loanCalculator.services'])
 
-  .directive('ccMonthlyPayments', ['calculateLoanService', function (calculateLoanService) {
-
+  .directive('ccLoanParameterInput', [function () {
     return {
       scope: {
-        amount: '=',
-        interest: '=',
-        downPayYears: '='
+        labelText: '@',
+        units: '@',
+        inputParameter: '='
       },
-      link: function (scope, element, attrs) {
-        var now = moment();
-        var args = {
-          loanRaisingMonth         : now.month(),
-          loanRaisingYear          : now.year(),
-          principalAmount          : scope.amount,
-          annualNominalInterestRate: scope.interest,
-          totalNumberOfPayments    : scope.downPayYears * 12
-        };
-        function calculateLoan(serviceArgs) {
-          calculateLoanService(serviceArgs).success(function (data) {
-            console.log('fetched data: ', data);
-            element.text(Math.round(data.amortizationSchedule[0].payment));
-          });
-        }
-        scope.$watch('amount', function (newVal) {
-          args.principalAmount = newVal;
-          calculateLoan(args);
-        });
-        scope.$watch('interest', function (newVal) {
-          args.annualNominalInterestRate = newVal;
-          calculateLoan(args);
-        });
-        scope.$watch('downPayYears', function (newVal) {
-          args.totalNumberOfPayments = newVal * 12;
-          calculateLoan(args);
-        });
-      }
+      templateUrl: 'partials/cc-loan-parameter-input.html'
     };
-
   }]);
+
