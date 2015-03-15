071519ebaa561f6dc0a3b2d7eca3fd306f209c42
Øystein Kjærnet <kjarnet@gmail.com>
2015-03-15 20:01:29 +0100

    Renamed directive and added an integer-validator-directive.

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,14 +2,28 @@
 
 angular.module('loanCalculator.directives', ['loanCalculator.controllers'])
 
-  .directive('ccLoanParameterInput', [function () {
+  .directive('ccInteger', function () {
+    return {
+      require: 'ngModel',
+      link   : function (scope, elm, attrs, ngModelController) {
+        ngModelController.$validators.ccInteger = function (modelValue, viewValue) {
+          if (ngModelController.$isEmpty(modelValue)) {
+            return true;// consider empty models to be valid
+          }
+          return !!/^\-?\d+$/.test(viewValue);
+        };
+      }
+    };
+  })
+
+  .directive('ccCalculatorForm', [function () {
     return {
       scope: {
         inputAmount: '=amount',
         inputDownPayYears: '=downPayYears',
         inputInterest: '=interest'
       },
-      templateUrl: 'partials/cc-loan-parameter-input.html',
+      templateUrl: 'partials/cc-calculator-form.html',
       controller: 'CalculatorFormCtrl'
     };
   }]);
